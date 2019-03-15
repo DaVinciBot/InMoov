9ef2e70a964b7a9daaef4665dd15ea7e68bed408
0xUKN <0xukn@protonmail.com>
2019-03-15 17:44:18 +0100

    new websocket client

--- internal_modules/websocket_api/websocket_client.py
+++ internal_modules/websocket_api/websocket_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import websockets, asyncio, sys
+import websockets, asyncio, sys, json
 
 async def send(uri, data):
 	async with websockets.connect(uri) as websocket:
@@ -11,4 +11,7 @@ async def send(uri, data):
 if len(sys.argv) != 2:
 	print("Usage : %s <data>" % sys.argv[0])
 	exit(0)
-asyncio.get_event_loop().run_until_complete(send('ws://192.168.1.107:8765',sys.argv[1]))
+with open(sys.argv[1], 'r') as f:
+	data = f.read()
+	print(data.encode('utf-8'))
+asyncio.get_event_loop().run_until_complete(send('ws://192.168.1.107:8765', data))
