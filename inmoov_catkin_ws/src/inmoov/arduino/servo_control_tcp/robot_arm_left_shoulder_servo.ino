104b83ce206c0952d3237f7e7acfa02cf4315ff3
None <None>
2019-06-06 11:21:02 +0000

    Manually merged with Flox branch

---
+++ inmoov_catkin_ws/src/inmoov/arduino/servo_control_tcp/robot_arm_left_shoulder_servo.ino
@@ -0,0 +1,69 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+#include <Servo.h>
+
+#define ROSSERIAL_ARDUINO_TCP
+
+#include <ros.h>
+#include <std_msgs/UInt8.h>
+
+/****************************************/
+/*************** CONFIG *****************/
+/****************************************/
+
+#define SERVO_PIN 9
+#define SERVO_ANGLE_MAX 120
+#define SERVO_ANGLE_MIN 80
+
+#define TOPIC_NAME "/robot/arm/left/shoulder/servo/angle"
+
+// TCP/IP settings
+IPAddress ip(10, 42, 0, 3);
+uint16_t serverPort = 11411;
+byte mac[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFA, 0xFF };
+
+/****************************************/
+/***************** END ******************/
+/****************************************/
+
+
+/************* DO NOT TOUCH *************/
+
+ros::NodeHandle  nh;
+
+Servo servo_;
+uint8_t angle_;
+
+// Server settings
+IPAddress server(10, 42, 0, 1);
+
+void onServoMsg( const std_msgs::UInt8& msg){
+  angle_ = msg.data;
+  
+  if( angle_ > SERVO_ANGLE_MAX ) angle_ = SERVO_ANGLE_MAX;
+  if( angle_ < SERVO_ANGLE_MIN ) angle_ = SERVO_ANGLE_MIN;
+
+  servo_.write(angle_);
+}
+
+ros::Subscriber<std_msgs::UInt8> sub(TOPIC_NAME, &onServoMsg );
+
+void setup()
+{
+  Ethernet.begin(mac, ip);
+  delay(1000);
+
+  servo_.attach(SERVO_PIN);
+  
+  nh.getHardware()->setConnection(server, serverPort);
+  nh.initNode();
+  
+  nh.subscribe(sub);
+}
+
+void loop()
+{
+  nh.spinOnce();
+  delay(1);
+}
